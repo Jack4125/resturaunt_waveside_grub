1fce2385f88aab7b5782d4935e1ff6f46c46f021
Jack <cba4125@outlook.com>
2021-01-22 00:10:49 -0800

    before change

--- script.js
+++ script.js
@@ -17,28 +17,28 @@
   });
 
   // nav control
-document.querySelector('.menu-btn').addEventListener('click', () => {
-  document.querySelector('.nav-list').classList.add('nav-show');
-  document.querySelector('.nav-control').classList.add('nav-control-show');
-});
+  document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-list').classList.add('nav-show');
+    document.querySelector('.nav-control').classList.add('nav-control-show');
+  });
 
-document.querySelector('.nav-control').addEventListener('click', () => {
-  document.querySelector('.nav-list').classList.remove('nav-show');
-  document.querySelector('.nav-control').classList.remove('nav-control-show');
-});
+  document.querySelector('.nav-control').addEventListener('click', () => {
+    document.querySelector('.nav-list').classList.remove('nav-show');
+    document.querySelector('.nav-control').classList.remove('nav-control-show');
+  });
 
-/* Hide header on scroll down. Show on scroll up */
-let prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
-  document.querySelector('.nav-list').classList.remove('nav-show');
-  document.querySelector('.nav-control').classList.remove('nav-control-show');
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.querySelector("header").style.top = "0";
-  } else {
-    document.querySelector("header").style.top = "-200px";
+  /* Hide header on scroll down. Show on scroll up */
+  let prevScrollPos = window.pageYOffset;
+  window.onscroll = function() {
+    document.querySelector('.nav-list').classList.remove('nav-show');
+    document.querySelector('.nav-control').classList.remove('nav-control-show');
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      document.querySelector("header").style.top = "0";
+    } else {
+      document.querySelector("header").style.top = "-200px";
+    }
+    prevScrollPos = currentScrollPos;
   }
-  prevScrollPos = currentScrollPos;
-}
 
 })();
